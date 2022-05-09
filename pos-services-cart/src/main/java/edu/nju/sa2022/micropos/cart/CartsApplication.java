9d801a4c6fcd96139d7cc37d04ef03c656c67ba3
Tianyun Zhang <doowzs@qq.com>
2022-05-09 10:23:04 +0800

    update docker-compose, use zookeeper as discovery

--- pos-services-cart/src/main/java/edu/nju/sa2022/micropos/cart/CartsApplication.java
+++ pos-services-cart/src/main/java/edu/nju/sa2022/micropos/cart/CartsApplication.java
@@ -2,10 +2,8 @@ package edu.nju.sa2022.micropos.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class CartsApplication {
 
     public static void main(String[] args) {
--- pos-services-product/src/main/java/edu/nju/sa2022/micropos/product/ProductsApplication.java
+++ pos-services-product/src/main/java/edu/nju/sa2022/micropos/product/ProductsApplication.java
@@ -3,10 +3,8 @@ package edu.nju.sa2022.micropos.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableCaching
 public class ProductsApplication {
     public static void main(String[] args) {
